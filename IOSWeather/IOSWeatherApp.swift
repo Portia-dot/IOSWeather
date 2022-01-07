cf3e5c050a5b9054dce476586d2a59f856badbcd
Modamori Oluwayomi <77457879+Portia-dot@users.noreply.github.com>
2022-01-06 19:58:22 -0800

    Initial Commit

---
+++ IOSWeather/IOSWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSWeatherApp.swift
+//  IOSWeather
+//
+//  Created by Oluwayomi M on 2022-01-06.
+//
+
+import SwiftUI
+
+@main
+struct IOSWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
